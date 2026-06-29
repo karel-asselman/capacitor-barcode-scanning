370a83630dad23b2c6b5070d1053c6dae99877d9
Karel Asselman <Karel.Asselman@lv.vlaanderen.be>
2026-06-29 17:11:51 +0200

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,37 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CapacitorBarcodeScanner, CapacitorBarcodeScannerCameraDirection, CapacitorBarcodeScannerScanOrientation, CapacitorBarcodeScannerTypeHintALLOption } from '@capacitor/barcode-scanner';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('capacitor-barcode-scanning');
+  barcodes: string[] = [];
+
+ async scan(): Promise<string | null> {
+    try {
+      const result = await CapacitorBarcodeScanner.scanBarcode({
+        hint: CapacitorBarcodeScannerTypeHintALLOption.ALL,        
+        scanText: 'scan a barcode',
+        scanInstructions: 'You know what to do',
+        web: {
+          showCameraSelection: true,
+          scannerFPS: 30,                   
+        },
+        cameraDirection: CapacitorBarcodeScannerCameraDirection.BACK,
+        scanOrientation: CapacitorBarcodeScannerScanOrientation.PORTRAIT,
+      });
+      console.log('Scan result:', result);
+      this.barcodes.push(result.ScanResult);
+
+      return result.ScanResult;
+    } catch (error) {
+      console.error('Error scanning barcode:', error); 
+      
+      return null;
+    }
+  } 
 }
